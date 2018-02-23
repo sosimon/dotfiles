0b46ceeb4dc51b808b245ccdb731dd19bd122445
Simon So <simonso@google.com>
2018-02-22 21:11:49 -0800

    update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,17 @@
 # Install tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+# Install pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# Install NERDTree
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+
+# Install monokai
+mkdir -p ~/.vim/colors && \
+curl -LSso ~/.vim/colors/monokai.vim https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim
+
+# Install smooth scrolling
+mkdir -p ~/.vim/autoload && \
+curl -LSso ~/.vim/autoload/smooth_scroll.vim https://raw.githubusercontent.com/terryma/vim-smooth-scroll/master/autoload/smooth_scroll.vim
